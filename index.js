80ced1a5251d6f59265f47437146668bfe88d01c
Sam Bouso <sbouso@gmail.com>
2021-12-08 23:45:28 -0600

    revert cors changes

--- index.js
+++ index.js
@@ -14,14 +14,9 @@ const app = express()
 app.disable('x-powered-by');
 
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 8089
 
-var corsOptions = {
-  origin: 'https://demo.authmosis.com',
-  optionsSuccessStatus: 204 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
-
-app.use(cors(corsOptions))
+app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
